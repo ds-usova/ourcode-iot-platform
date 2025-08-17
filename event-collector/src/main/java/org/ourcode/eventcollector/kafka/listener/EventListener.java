0f71ad452ba5a8d7eb470a90eeb41d39a94fe1ad
ds-usova <ds.usova@mail.ru>
2025-08-17 23:51:48 +0200

    - init event-collector - connect to events topic

---
+++ event-collector/src/main/java/org/ourcode/eventcollector/kafka/listener/EventListener.java
@@ -0,0 +1,17 @@
+package org.ourcode.eventcollector.kafka.listener;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EventListener {
+
+    @KafkaListener(topics = "events", groupId = "${spring.kafka.consumer.group-id}")
+    public void listen(ConsumerRecord<String, String> record, Acknowledgment ack) {
+        System.out.println("Received event: key " + record.key() + " value " + record.value());
+        ack.acknowledge();
+    }
+
+}
