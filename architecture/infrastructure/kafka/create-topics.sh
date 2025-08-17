c7f3be5af353c402d672a39eb0f6102d3e989cdd
ds-usova <ds.usova@mail.ru>
2025-08-17 22:45:40 +0200

    Init kafka topics

---
+++ architecture/infrastructure/kafka/create-topics.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+BOOTSTRAP_SERVER=kafka:9092
+KAFKA_BIN=/opt/bitnami/kafka/bin
+
+echo "Creating Kafka topics on $BOOTSTRAP_SERVER ..."
+
+$KAFKA_BIN/kafka-topics.sh --create --if-not-exists \
+  --bootstrap-server "$BOOTSTRAP_SERVER" \
+  --replication-factor 1 \
+  --partitions 3 \
+  --topic events
+echo "Topic 'events' created (or already existed)."
+
+echo "All topics created (or already existed)."
