d128e222ab48f84cf37194cba33a5a6c5ac1b7b9
ds-usova <ds.usova@mail.ru>
2025-11-28 00:13:39 +0100

    Measure db queries execution time and retry if required

--- device-service/src/main/java/org/ourcode/deviceservice/persistence/PostgresDeviceGateway.java
+++ device-service/src/main/java/org/ourcode/deviceservice/persistence/PostgresDeviceGateway.java
@@ -1,5 +1,6 @@
 package org.ourcode.deviceservice.persistence;
 
+import io.micrometer.core.annotation.Timed;
 import lombok.extern.slf4j.Slf4j;
 import org.ourcode.deviceservice.api.exception.DuplicateException;
 import org.ourcode.deviceservice.api.gateway.DeviceGateway;
@@ -8,7 +9,6 @@ import org.ourcode.deviceservice.persistence.configuration.TranslatePersistenceE
 import org.ourcode.deviceservice.persistence.entity.DeviceEntity;
 import org.ourcode.deviceservice.persistence.repository.DeviceRepository;
 import org.ourcode.deviceservice.util.TimeUtils;
-import org.springframework.dao.DataAccessException;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
@@ -18,6 +18,11 @@ import java.util.Optional;
 
 @Slf4j
 @Component
+@Retryable(
+        noRetryFor = { DuplicateException.class },
+        maxAttemptsExpression = "${spring.retry.device-gateway.max-attempts}",
+        backoff = @Backoff(delayExpression = "${spring.retry.device-gateway.backoff-delay}")
+)
 @TranslatePersistenceExceptions
 public class PostgresDeviceGateway implements DeviceGateway {
 
@@ -28,12 +33,8 @@ public class PostgresDeviceGateway implements DeviceGateway {
     }
 
     @Override
-    @Retryable(
-            retryFor = DataAccessException.class,
-            maxAttemptsExpression = "${spring.retry.device-gateway.max-attempts}",
-            backoff = @Backoff(delayExpression = "${spring.retry.device-gateway.backoff-delay}")
-    )
     @Transactional
+    @Timed(value = "device.create.time", description = "Time taken to create a device")
     public Device create(Device device) {
         log.debug("Creating device with ID {}", device.id());
 
@@ -44,6 +45,7 @@ public class PostgresDeviceGateway implements DeviceGateway {
 
     @Override
     @Transactional(readOnly = true)
+    @Timed(value = "device.read.time", description = "Time taken to get a device by id")
     public Optional<Device> getBy(String deviceId) {
         log.debug("Getting device with ID {}", deviceId);
         return deviceRepository.findById(deviceId).map(DeviceEntity::toModel);
@@ -51,6 +53,7 @@ public class PostgresDeviceGateway implements DeviceGateway {
 
     @Override
     @Transactional
+    @Timed(value = "device.update.time", description = "Time taken to update a device")
     public Optional<Device> update(String deviceId, String type, String meta) {
         log.debug("Updating device with ID {}", deviceId);
         return deviceRepository.updateDevice(deviceId, type, meta).map(DeviceEntity::toModel);
@@ -58,6 +61,7 @@ public class PostgresDeviceGateway implements DeviceGateway {
 
     @Override
     @Transactional
+    @Timed(value = "device.delete.time", description = "Time taken to delete a device")
     public boolean delete(String deviceId) {
         log.debug("Deleting device with ID {}", deviceId);
         return deviceRepository.deleteBy(deviceId) == 1;
